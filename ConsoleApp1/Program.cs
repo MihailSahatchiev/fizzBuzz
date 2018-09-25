2754690c0e9865f0b07ec9fa0a04f698083bd3ba
mihail.sahatchiev <msahatchiev@melontech.com>
2018-09-25 13:32:37 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string limit = Console.ReadLine();
+            uint limitConvert = uint.Parse(limit);
+            if (limitConvert > 0)
+            {
+                for (uint i = 1; i <= limitConvert; i++)
+                {
+                    if (i % 3 == 0 && i % 5 == 0)
+                    {
+                        Trace.WriteLine("FizzBuzz");
+                    }
+                    else if (i % 3 == 0)
+                    {
+                        Trace.WriteLine("Fizz");
+                    }
+                    else if (i % 5 == 0)
+                    {
+                        Trace.WriteLine("Buzz");
+                    }
+                    else
+                    {
+                        Trace.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
+}
